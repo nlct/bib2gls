680f9432c95b2c587ec4b258125f5e008479ec3f
nlct <n.talbot@uea.ac.uk>
2017-02-04 20:32:42 +0000

    fixed --no-interpret option

--- src/java/Bib2Gls.java
+++ src/java/Bib2Gls.java
@@ -2652,7 +2652,7 @@ public class Bib2Gls implements TeXApp
    }
 
    public static final String NAME = "bib2gls";
-   public static final String VERSION = "0.2a";
+   public static final String VERSION = "0.3a";
    public static final String DATE = "EXPERIMENTAL";
    //public static final String DATE = "2017-02-04";
    public int debugLevel = 0;
