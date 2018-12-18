9586321663d074edb7064aee777fafe8d14db4f4
nlct <n.talbot@uea.ac.uk>
2018-12-18 12:17:13 +0000

    added check for MissingValue

--- src/java/bib2gls/Bib2Gls.java
+++ src/java/bib2gls/Bib2Gls.java
@@ -4911,8 +4911,8 @@ public class Bib2Gls implements TeXApp
    }
 
    public static final String NAME = "bib2gls";
-   public static final String VERSION = "1.8.20181217";
-   public static final String DATE = "2018-12-17";
+   public static final String VERSION = "1.8.20181218";
+   public static final String DATE = "2018-12-18";
    public int debugLevel = 0;
    public int verboseLevel = 0;
 
