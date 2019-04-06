3a9a7ecc5802e2a63a2b872d5d22aea14a92ef1c
nlct <n.talbot@uea.ac.uk>
2019-04-06 20:56:15 +0100

    added vert and minor edits

--- src/java/bib2gls/Bib2Gls.java
+++ src/java/bib2gls/Bib2Gls.java
@@ -5045,8 +5045,8 @@ public class Bib2Gls implements TeXApp
    }
 
    public static final String NAME = "bib2gls";
-   public static final String VERSION = "1.8.20190401";
-   public static final String DATE = "2019-04-01";
+   public static final String VERSION = "1.8.20190406";
+   public static final String DATE = "2019-04-06";
    public int debugLevel = 0;
    public int verboseLevel = 0;
 
