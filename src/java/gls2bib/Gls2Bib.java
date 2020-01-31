4c89d89544457c574d6927aaa0ecc9345da69331
nlct <n.talbot@uea.ac.uk>
2020-01-31 23:30:30 +0000

    convertgls2bib: change overwrite for \renewcommand

--- src/java/gls2bib/Gls2Bib.java
+++ src/java/gls2bib/Gls2Bib.java
@@ -181,6 +181,10 @@ public class Gls2Bib extends LaTeXParserListener
    {
       super.addPredefined();
 
+      // don't complain about redefining unknown commands
+      parser.putControlSequence(new NewCommand("renewcommand",
+        NewCommand.OVERWRITE_ALLOW));
+
       parser.putControlSequence(new NewGlossaryEntry(this));
       parser.putControlSequence(new NewGlossaryEntry(
         "newentry", this));
@@ -1362,8 +1366,8 @@ public class Gls2Bib extends LaTeXParserListener
       expandFieldMap.put(field, Boolean.valueOf(on));
    }
 
-   public static final String VERSION = "1.9.20200130";
-   public static final String DATE = "2020-01-30";
+   public static final String VERSION = "1.9.20200131";
+   public static final String DATE = "2020-01-31";
    public static final String APP_NAME = "convertgls2bib";
 
    private Vector<GlsData> data;
