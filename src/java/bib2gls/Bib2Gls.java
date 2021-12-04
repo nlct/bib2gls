44339c21792d9573e0f9519ddcf5d664baff2caf
nlct <n.talbot@uea.ac.uk>
2021-12-04 16:49:29 +0000

    updated manual to include hierarchical groups info

--- src/java/bib2gls/Bib2Gls.java
+++ src/java/bib2gls/Bib2Gls.java
@@ -5787,8 +5787,8 @@ public class Bib2Gls implements TeXApp
    }
 
    public static final String NAME = "bib2gls";
-   public static final String VERSION = "3.0.20211203";
-   public static final String DATE = "2021-12-03";
+   public static final String VERSION = "3.0.20211204";
+   public static final String DATE = "2021-12-04";
    public int debugLevel = 0;
    public int verboseLevel = 0;
 
