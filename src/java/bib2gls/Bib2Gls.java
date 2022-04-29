bd6ca769c03cdac1fcac93c31223543ba0942fea
nlct <n.talbot@uea.ac.uk>
2022-04-29 23:02:39 +0100

    updated date

--- src/java/bib2gls/Bib2Gls.java
+++ src/java/bib2gls/Bib2Gls.java
@@ -5786,8 +5786,8 @@ public class Bib2Gls implements TeXApp
    }
 
    public static final String NAME = "bib2gls";
-   public static final String VERSION = "3.0.20211214";
-   public static final String DATE = "2021-12-14";
+   public static final String VERSION = "3.0.20220429";
+   public static final String DATE = "2022-04-29";
    public int debugLevel = 0;
    public int verboseLevel = 0;
 
