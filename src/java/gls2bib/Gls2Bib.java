719d51e619f2750b6c6ef5358f895ed409b1b121
nlct <n.talbot@uea.ac.uk>
2023-06-29 19:56:50 +0100

    version 3.4

--- src/java/gls2bib/Gls2Bib.java
+++ src/java/gls2bib/Gls2Bib.java
@@ -1814,8 +1814,8 @@ public class Gls2Bib extends LaTeXParserListener
       expandFieldMap.put(field, Boolean.valueOf(on));
    }
 
-   public static final String VERSION = "3.3";
-   public static final String DATE = "2023-04-24";
+   public static final String VERSION = "3.4";
+   public static final String DATE = "2023-06-29";
    public static final String NAME = "convertgls2bib";
 
    private Vector<GlsData> data;
